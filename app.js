062c55a72e446e44858612226515923c0e863f8c
Viktoriia <pecourses>
2023-02-26 20:16:29 +0200

    Add some comments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const { format } = require('date-fns');
 
+// Тестові початкові дані
 const contactsDB = [
   {
     id: 0,
@@ -18,49 +19,32 @@ const contactsDB = [
     isFavourite: true,
   },
 ];
+
+// Клас для доступу до масива
 class ContactsDB {
   constructor (arr) {
     this.contacts = [...arr];
   }
 
+  // Метод для додавання нового об'єкта в масив: додає id і isFavourite
   createContact (newContact) {
     this.contacts.push({ ...newContact, id: uuidv4(), isFavourite: false });
     return this.contacts[this.contacts.length - 1];
   }
 
+  // Метод для отримання даних з масиву
   getContacts () {
     return [...this.contacts];
   }
-
-  getContactById (id) {
-    const foundIndex = this.contacts.findIndex(c => c.id === Number(id));
-    return foundIndex === -1 ? null : this.contacts[foundIndex];
-  }
-
-  updateContact (id, values) {
-    const foundContactIndex = this.contacts.findIndex(c => c.id === Number(id));
-    if (foundContactIndex !== -1) {
-      this.contacts[foundContactIndex] = {
-        ...this.contacts[foundContactIndex],
-        ...values,
-      };
-    }
-
-    return foundContactIndex === -1 ? null : this.contacts[foundContactIndex];
-  }
-
-  deleteContact (id) {
-    const foundContactIndex = this.contacts.findIndex(c => c.id === Number(id));
-
-    return foundContactIndex === -1
-      ? null
-      : this.contacts.splice(foundContactIndex, 1);
-  }
 }
+
+// Інстанс класу для доступу до масиву з об'єктами
 const contactsDbInstace = new ContactsDB(contactsDB);
 
+// Створення екземпляру експресу
 const app = express();
 
+// Middleware to parse json to js-object
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -68,18 +52,17 @@ app.get('/', (req, res) => {
 });
 
 // CRUD
+// Навішування обробника на метод GET на маршрут '/contacts'
 app.get('/contacts', (req, res) => {
   const contacts = contactsDbInstace.getContacts();
   res.status(200).send(contacts);
 });
 
+// Навішування обробника на метод POST на маршрут '/contacts'
 app.post('/contacts', (req, res) => {
+  // В req.body приходе тіло запиту
   const createdContact = contactsDbInstace.createContact(req.body);
   res.status(201).send(createdContact);
 });
-
-app.get('/contacts/1', (req, res) => {});
-app.patch('/contacts/1', (req, res) => {});
-app.delete('/contacts/1', (req, res) => {});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const app = require('./app');
 
+// Визначаємо номер порта зі змінних середовища (якщо задано) або по дефолту 5000
 const PORT = process.env.PORT || 5000;
 
+// Створюємо сервер і призначаємо у якості обробника app
 const httpServer = http.createServer(app);
 
+// Запускаємо сервер на прослуховування порту 5000
 httpServer.listen(PORT, () => {
   console.log(`Server is listening http://localhost:${PORT}`);
 });
