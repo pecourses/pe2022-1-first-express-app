a4558104a6e0360bc504c31e85c8139bada729f4
Viktoriia <pecourses>
2023-03-04 16:19:27 +0200

    Add params, query

--- app.js
+++ app.js
@@ -1,68 +1,51 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const { format } = require('date-fns');
 
-// Тестові початкові дані
-const contactsDB = [
-  {
-    id: 0,
-    name: 'Test',
-    telNumber: '+380123456789',
-    birthday: '2000-12-01',
-    isFavourite: false,
-  },
-  {
-    id: 1,
-    name: 'Test1',
-    telNumber: '+380123456788',
-    birthday: format(new Date(), 'Y-MM-dd'),
-    isFavourite: true,
-  },
-];
-
-// Клас для доступу до масива
-class ContactsDB {
-  constructor (arr) {
-    this.contacts = [...arr];
-  }
-
-  // Метод для додавання нового об'єкта в масив: додає id і isFavourite
-  createContact (newContact) {
-    this.contacts.push({ ...newContact, id: uuidv4(), isFavourite: false });
-    return this.contacts[this.contacts.length - 1];
-  }
-
-  // Метод для отримання даних з масиву
-  getContacts () {
-    return [...this.contacts];
-  }
-}
-
-// Інстанс класу для доступу до масиву з об'єктами
-const contactsDbInstace = new ContactsDB(contactsDB);
-
-// Створення екземпляру експресу
 const app = express();
 
-// Middleware to parse json to js-object
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('app )))');
 });
 
-// CRUD
-// Навішування обробника на метод GET на маршрут '/contacts'
-app.get('/contacts', (req, res) => {
+app.get('/contacts/', (req, res) => {
   const contacts = contactsDbInstace.getContacts();
   res.status(200).send(contacts);
 });
 
-// Навішування обробника на метод POST на маршрут '/contacts'
 app.post('/contacts', (req, res) => {
-  // В req.body приходе тіло запиту
   const createdContact = contactsDbInstace.createContact(req.body);
   res.status(201).send(createdContact);
 });
 
+// params, req
+// GET localhost:5000/contacts/10?results=10&page=5
+// :id - параметр маршрута
+// ?results=10&page=5 - параметри рядка запиту
+app.get('/contacts/:id', (req, res) => {
+  const {
+    params: { id },
+    query: { results, page },
+  } = req;
+
+  console.log('req.params :>> ', req.params);
+  console.log('req.query :>> ', req.query);
+
+  res.status(200).send('OK');
+});
+
 module.exports = app;
+
+// звернутися до всіх замовлень певного користувача, зокрема до виконаних
+// (від загального до конретного)
+// users/5/orders?isDone=true
+// прописати обробник, в якому витягти id користувача і дані для фільтру замовлень
+app.get('/users/:id/orders', (req, res) => {
+  const {
+    params: { id },
+    query: { isDone },
+  } = req;
+
+  console.log('id, isDone :>> ', id, isDone);
+  res.status(200).send('OK');
+});
